309655766a8f082ab22d9f1737bc732ca754955e
Juan Pablo Guaglianone <juanpiguaglianone@gmail.com>
2024-06-16 22:44:20 -0300

    Algunos cambios en cEstoico

--- Project1/cEstoico.cpp
+++ Project1/cEstoico.cpp
@@ -1,5 +1,8 @@
 #include "Headers.h"
 
+int cEstoico::vikingosActivos = 0;
+int cEstoico::dragonesActivos = 0;
+
 list<cDragon*> cEstoico::getDragones()
 {
 	return dragones;
@@ -13,6 +16,53 @@ list<cJinete*> cEstoico::getJinetes()
 list<cVikingo*> cEstoico::getVikingos()
 {
 	return vikingos;
+}
+
+void cEstoico::AgregarDragon(cDragon* dra_nue)
+{
+	this->dragones.push_back(dra_nue);
+	dragonesActivos++;
+}
+
+void cEstoico::AgregarVikingo(cVikingo* vik_nue)
+{
+	this->vikingos.push_back(vik_nue);
+	vikingosActivos++;
+}
+
+void cEstoico::EliminarVikingo(cVikingo* vik_elim)
+{
+
+}
+
+void cEstoico::EliminarDragon(cDragon* dra_elim)
+{
+}
+
+void cEstoico::operator+(cDragon* dra_nue)
+{
+	AgregarDragon(dra_nue);
+}
+
+void cEstoico::operator+(cVikingo* vik_nue)
+{
+	AgregarVikingo(vik_nue);
+}
+
+void cEstoico::operator-(cDragon* dra_elim)
+{
+	if (dra_elim != nullptr)
+		EliminarDragon(dra_elim);
+	else
+		throw exception("No se puede eliminar, vacio"); //hacer el try catch
+}
+
+void cEstoico::operator-(cVikingo* vik_elim)
+{
+	if (vik_elim != nullptr)
+		EliminarVikingo(vik_elim);
+	else
+		throw exception("No se puede eliminar, vacio"); //hacer el try catch
 }
 
 cEstoico::cEstoico(cDragon* dragon1, cJinete* jinete1, cVikingo* vikingo1)
--- Project1/cEstoico.h
+++ Project1/cEstoico.h
@@ -3,13 +3,27 @@
 class cEstoico
 {
 private:
+	static int vikingosActivos;
+	static int dragonesActivos;
 	list <cDragon*> dragones;
-	list <cJinete*> jinetes;
+	list <cJinete*> jinetes; //Sacar cuando hagamos la herencia
 	list <cVikingo*> vikingos;
 public:
 	list <cDragon*> getDragones(); 
-	list <cJinete*> getJinetes(); 
+	list <cJinete*> getJinetes(); //Sacara cuando hagamos herencia
 	list <cVikingo*> getVikingos(); 
+	void AgregarDragon(cDragon* dra_nue);
+	void AgregarVikingo(cVikingo* vik_nue);
+	void EliminarVikingo(cVikingo* vik_elim);
+	void EliminarDragon(cDragon* dra_elim);
+
+	void operator+(cDragon* dra_nue);
+	void operator+(cVikingo* vik_nue); 
+	void operator-(cDragon* dra_elim);
+	void operator-(cVikingo* vik_elim);
+
+	//agregar funciones que hagan lo que pide la consigna
+
 	cEstoico();
 	cEstoico(cDragon* dragon1, cJinete* jinete1, cVikingo* vikingo1);
 	~cEstoico();
